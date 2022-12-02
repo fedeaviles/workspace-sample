fc5552c95a4ad7759fe3d9207e03152f3adda03f
Federico Aviles <federico.aviles@ripio.com>
2022-12-02 16:58:58 -0300

    add test

--- packages/is-odd/index.spec.ts
+++ packages/is-odd/index.spec.ts
@@ -5,4 +5,8 @@ describe("isOdd function", () => {
     const result = isOdd(1);
     expect(result).toBe(true);
   });
+  it("should return false", () => {
+    const result = isOdd(2);
+    expect(result).toBe(false);
+  });
 });
